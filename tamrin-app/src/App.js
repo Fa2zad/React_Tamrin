8e088b681aa5294b8c4f33f4ff356680ef2ffd48
محسن نیکنام <niknam@faratech.com>
2018-09-05 11:03:35 +0430

    Fix bootstrap

--- tamrin-app/src/App.js
+++ tamrin-app/src/App.js
@@ -13,6 +13,15 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-4">q</div>
+            <div className="col-md-4">b</div>
+            <div className="col-md-4">c</div>
+          </div>
+        </div>
+        <div className="alert alert-success">aaaa</div>
+
       </div>
     );
   }
